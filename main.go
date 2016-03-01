df5a7591ccd99b14fd5ea123d85e288308bc4fc9
Daniel Kirchner <daniel.c.kirchner@googlemail.com>
2016-03-01 07:25:18 -0800

    solve conflict in main

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"HAWAI/repos/hawai-runner/runner"
 )
 
 func main() {
 	runner.Main()
-=======
-	"HAWAI/repos/hawai-logginghub/logginghub"
-)
-
-func main() {
-	logginghub.Main()
->>>>>>> 4ba6d0535ed527f42a9f3736c8699a759ed6cd58
 }
